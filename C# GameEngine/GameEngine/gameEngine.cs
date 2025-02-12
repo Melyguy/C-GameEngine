c5a2a13e3c753fa7d9fad184cacfecba1392eadd
MellyDev <nidojohan@gmail.com>
2025-02-12 23:56:42 +0100

    commit message👍

---
+++ C# GameEngine/GameEngine/gameEngine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using C__GameEngine.GameEngine; // Ensure this line is present
+
+namespace C__GameEngine.C_engine
+{
+    class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+
+    public abstract class gameEngine
+    {
+        private Vector2 ScreenSize = new Vector2(512, 512);
+        private string Title = "Game";
+        private Canvas Window;
+
+        public gameEngine(Vector2 ScreenSize, string Title)
+        {
+            this.ScreenSize = ScreenSize;
+            this.Title = Title;
+
+            Window = new Canvas();
+            Window.Size = new Size((int)this.ScreenSize.X, (int)this.ScreenSize.Y);
+            Window.Text = this.Title;
+
+            Application.Run(Window);
+        }
+    }
+}
--- C# GameEngine/Program.cs
+++ C# GameEngine/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using C__GameEngine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace C_Sharp_GameEngine
     {
         static void Main(string[] args)
         {
+            DemoGame game = new DemoGame();
         }
     }
 }
